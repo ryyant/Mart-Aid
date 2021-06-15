f664ccea1023f5c1a7c64922d597e2968e8cd311
ryyant <ryyant@hotmail.com>
2021-06-15 12:38:23 +0800

    navigation structure tabs

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ import AppLoading from 'expo-app-loading';
 import LoginScreen from "./src/screens/LoginScreen";
 import SignUpScreen from "./src/screens/SignUpScreen";
 import LoadingScreen from "./src/screens/LoadingScreen";
-import RequestsScreen from "./src/screens/RequestsScreen";
+import BottomBar from "./src/components/BottomBar";
 
 import { useFonts, Merriweather_700Bold, Merriweather_400Regular } from '@expo-google-fonts/merriweather';
 
@@ -18,7 +18,7 @@ const screens = [
   { name: "Loading", component: LoadingScreen },
   { name: "SignUp", component: SignUpScreen },
   { name: "Login", component: LoginScreen },
-  { name: "Requests", component: RequestsScreen },
+  { name: "Main", component: BottomBar },
 ];
 
 LogBox.ignoreLogs(["Setting a timer for a long period of"]);
---
+++ src/screens/ProfileScreens/YourListingScreen.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+  Header,
+} from "react-native";
+import Screen from '../../components/Screen';
+
+export default function ({ navigation }) {
+
+  return (
+    <Screen styles = {styles.container}>
+      <Text>Your Listings</Text>
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "blue",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
